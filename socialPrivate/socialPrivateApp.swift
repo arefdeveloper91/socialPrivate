b5642fd13f588efbab0f13a9d6688f13bdab6101
aref <devfera14@gmail.com>
2025-09-03 11:30:44 -0300

    Initial Commit

---
+++ socialPrivate/socialPrivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  socialPrivateApp.swift
+//  socialPrivate
+//
+//  Created by arefdeveloper on 03/09/25.
+//
+
+import SwiftUI
+
+@main
+struct socialPrivateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
