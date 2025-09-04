706e276bbd371c0909d335561cad615cf74d5ef3
aref <devfera14@gmail.com>
2025-09-04 11:33:41 -0300

    finalizando a loginView com direcionamento do navigationLink para forgot password e para registerView

--- socialPrivate/Core/Authentication/Views/LoginView.swift
+++ socialPrivate/Core/Authentication/Views/LoginView.swift
@@ -8,8 +8,76 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                Image("App")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 300)
+                    .padding()
+                
+                VStack{
+                    TextField("Enter with your email", text: $email)
+                        .padding(12)
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                        .padding(.horizontal,10)
+                    SecureField("Enter with your password", text: $password)
+                        .padding(12)
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                        .padding(.horizontal,10)
+                    
+                }
+                NavigationLink{
+                    Text("Forgot Password?")
+                }label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing,28)
+                        .foregroundColor(.blue)
+                        .frame(maxWidth:.infinity , alignment: .trailing)
+                }
+                Button{
+                    
+                }label:{
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.blue)
+                        .frame(width: 352, height: 44)
+                        .background(.black)
+                        .cornerRadius(8)
+                        
+                }
+                Spacer()
+                Divider()
+                
+                NavigationLink {
+                    Text("Registration View")
+                }label: {
+                    HStack{
+                        Text("Dont have account?")
+                        
+                        Text ("Sign Up")
+                            .fontWeight(.semibold)
+                        
+                        
+                    }
+                    .foregroundColor(.black)
+                    .font(.footnote)
+                }
+                .padding(.vertical,16)
+            }
+            
+        }
     }
 }
 
