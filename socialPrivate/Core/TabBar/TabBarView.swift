8ca1d3711b26f2ff283752a1eaf2e6efbb9cb72a
aref <devfera14@gmail.com>
2025-09-04 16:16:55 -0300

    TabBar quase concluida

---
+++ socialPrivate/Core/TabBar/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  socialPrivate
+//
+//  Created by arefdeveloper on 04/09/25.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
