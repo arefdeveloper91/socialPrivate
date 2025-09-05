0db6a6c34c21b642d1e0380c997b1c6f9c94db72
aref <devfera14@gmail.com>
2025-09-05 10:17:47 -0300

    iniciando a parte do feed

---
+++ socialPrivate/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  socialPrivate
+//
+//  Created by arefdeveloper on 04/09/25.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text("Hello Feed")
+    }
+}
+
+#Preview {
+    FeedView()
+}
---
+++ socialPrivate/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadCell.swift
+//  socialPrivate
+//
+//  Created by arefdeveloper on 05/09/25.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
